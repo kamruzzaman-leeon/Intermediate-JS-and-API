0eaa5e6c212a00c21d58e48cc3f21767373e3d6e
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-09-01 06:47:49 +0600

    load posts and display on the webdite with css

--- js/post.js
+++ js/post.js
@@ -7,15 +7,26 @@ function loadPosts(){
      .then(data => displayData(data))
 }
 
-1.
+/*
+ 1.gety the container element where you want to add the new elements
+2. create child element
+3. set innerText or innerHTML
+4. appendChild
+*/
 
 function displayData(posts){
     const container = document.getElementById('posts-container');
     for(const post of posts){
-        const div = document.createElement('div');
-        div.innerHTML = `
+        const postdiv = document.createElement('div');
+        postdiv.classList.add('post'); 
+        postdiv.innerHTML = `
         <h5>Use ID: ${post.userId}</h5>
-        `
+        <h4>ID: ${post.id}</h4>
+        <h4>title: ${post.title}</h4>
+        <p>Description: ${post.body}</p>
+        `;
+
+        posts-container.appendChild(postdiv);
         console.log(posts);
     }
 }
